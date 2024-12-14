e44e5a75bf09b910b1d0ee5ebe7006fa90e19c44
punitsirse123 <punitsirse@gmail.com>
2024-12-15 03:12:29 +0530

    Add files via upload

---
+++ src/components/playground/icons.tsx
@@ -0,0 +1,39 @@
+export const CheckIcon = () => (
+  <svg
+    xmlns="https://drive.google.com/file/d/10ymMuVFzUDHn-ZbWxnHcuUqjHmfwIUGQ/view?usp=sharing"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+  >
+    <g clipPath="url(#clip0_718_9977)">
+      <path
+        d="M1.5 7.5L4.64706 10L10.5 2"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="square"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_718_9977">
+        <rect width="12" height="12" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const ChevronIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    fill="none"
+    xmlns="https://drive.google.com/file/d/10ymMuVFzUDHn-ZbWxnHcuUqjHmfwIUGQ/view?usp=sharing"
+    className="fill-gray-200 transition-all group-hover:fill-white group-data-[state=open]:rotate-180"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m8 10.7.4-.3 4-4 .3-.4-.7-.7-.4.3L8 9.3 4.4 5.6 4 5.3l-.7.7.3.4 4 4 .4.3Z"
+    />
+  </svg>
+);
